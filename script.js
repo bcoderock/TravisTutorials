bf3a147f137a32284cfaddfb7c696991cebee063
Bernice Kipkeny <bcoderock@gmail.com>
2024-10-03 07:55:34 -0400

    variables

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-//Ways to declare a var
+//Ways to declare a variable
+//var,let,const
+
+let firstName='Beano';
+let secondName='Kip';
+
+console.log(firstName,secondName);
+
+let age =30 ;
+
+console.log(age);
+
+// reassignment of variable
+
+age =40 ;
+
+console.log(age);
+
+let score;
+
+score = 1;
+
+console.log(score);
+
+if(true){
+    score= score+1;
+}
+
+console.log(score);
+
+/*const x =100 ;
+ x=200;
+Error Code will appear coz const is for constant variables.Const variables must be initialized
+ */
+
+const arr=[1,2,3,4];
+
+console.log(arr);
+
+arr.push(7);
+
+console.log(arr);
+
+
+const person ={
+    name:'Brandy',
+};
+
+console.log(person);
+
+person.name = 'Samera';
+
+person.email ='samera123@gmail.com';
+
+console.log(person);
+
+
+
