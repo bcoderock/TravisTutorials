92ecfcc2427d7613b94709ad5fd76da314c79e76
Bernice Kipkeny <bcoderock@gmail.com>
2024-10-07 21:50:37 -0400

    reference types

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-//strings 
-const output= 'Sarah';
-console.log(output, typeof output);
+//String
+const firstName ='sarah';
+
+//Number 
+const age =30 ;
+const temp = 98.9;
+
+//Boolean
+const hadKids =false ;
+
+//Null
+const aptNumber = null ;
+
+//Undefined 
+//let score ;
+const score = undefined ;
+
+//Symbol
+const id = Symbol('id');
+
+//BigInt
+const n =92929010010101900190291029019209102n;
+
+//Reference types
+const numbers =[1,2,3,4];
+const person={
+    name:'Brad',
+};
+
+const output = numbers;
+
+
+console.log(output,typeof output);
+
 
     
