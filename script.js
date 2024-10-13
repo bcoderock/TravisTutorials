5bd73f50cd6f5d46ae2a68ac61c30d9bdd0e8e20
Bernice Kipkeny <bcoderock@gmail.com>
2024-10-13 15:35:59 -0400

    Add NaN numbers

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let score ='100';
-//methods
-//score=parseInt(score);
-//score=+score;
-score=Number(score);
+let score ='Heloo';
 
+// Convert string to number let score = '100';
+score=parseInt(score);
+//score=+score;
+//score=Number(score);
+
+//Convert number to string let score = 100;
+//score=score.toString();
+//score=String(score);
+
+//Convert string to decimal let score = '100.1';
+//score=parseFloat(score);
+
+//Convert string to boolean let score =1;
+//score = Boolean(score);
+
+//We also learnt about Nan 'number'
 
 
 console.log(score,typeof score);
