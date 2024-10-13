a3185a5e8db1cbfee2757437328dedee993b84d4
Bernice Kipkeny <bcoderock@gmail.com>
2024-10-13 16:37:54 -0400

    assigment operators

---
+++ script.js
@@ -0,0 +1,41 @@
+//Arithmetic Operation
+
+let x ;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;//remainder
+
+//1. Concatenation +
+y ='Hello'+ ' ' +'World';
+
+//2.Exponent
+z = 2 **3;
+
+//3.Increment
+a=1;
+//a=a+1;
+a++;
+
+//4.Decrease
+b=0;
+//b=b-1;
+b--;
+
+//Assigment Operators
+ c = 10;
+
+ c += 5;
+ c = c+6;
+ c -= 8;
+ c *= 5;
+ 
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b);
+console.log(c);
