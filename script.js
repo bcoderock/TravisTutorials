fa7ff122326bc20781bb6612b055460dc3bfc5f5
Bernice Kipkeny <bcoderock@gmail.com>
2024-10-14 22:58:17 -0400

    strings

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //Strings
+
+let x;
+let y;//initialization
+let a;
+
+const name = 'Bernice';//Set a variable
+const name2 ='Jason';
+
+const age = 30 ;
+const age2 = 34;
+
+x ='My name is ' + name +' I am ' + age +' years old.';//Concatination
+
+//Template Literals using backTicks `
+y =`My name is ${name2} and I am ${age2} years`;//Remember to use the backticks
+
+//String Properties and Methods
+ const s ='Hello World'
+ 
+ z = s.length;
+ 
+ //Access value by key
+ z = s[3];
+
+ a = s._proto_;
+
+ a = s.toUpperCase();
+
+ 
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
